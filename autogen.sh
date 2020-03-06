4c0ffe92e3f3ca8fb55e7d7a295ca9c94d9c8136
richman <kozynets@gmail.com>
2020-03-06 14:30:57 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,10 +4,13 @@
 # You need automake 1.7 or later. 1.6 might work.
 
 set -e
-
-aclocal
-autoheader
-automake --foreign --add-missing --force-missing --copy
-# automake --gnu --add-missing --copy
-autoconf
+srcdir="$(dirname $0)"
+cd "$srcdir"
+if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
+which autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
+autoreconf --install --force --warnings=all
 
